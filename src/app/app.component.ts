bb26b77864aee0e01e95dba13182f0c367447528
Sanjd <jojoloiseaumusique@gmail.com>
2023-01-24 19:36:18 +0100

    Systeme de score

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,26 +10,26 @@ export class AppComponent {
   title = 'table-de-rappel';
 
   public static TABLE_DE_RAPPEL: {nombre: number, valeur: string}[] = [
-    {nombre: 0, valeur: "toto"},
-    {nombre: 1, valeur: "fleche"},
-    {nombre: 2, valeur: "nez"},
-    {nombre: 3, valeur: "handspinner"},
-    {nombre: 4, valeur: "rubixcube"},
-    {nombre: 5, valeur: "cheveux banane"},
-    {nombre: 6, valeur: "scie"},
-    {nombre: 7, valeur: "maman"},
-    {nombre: 8, valeur: "bonbon"},
-    {nombre: 9, valeur: "papa"},
-    {nombre: 10, valeur: "de"},
+    {nombre: 0, valeur: "Toto"},
+    {nombre: 1, valeur: "Flèche"},
+    {nombre: 2, valeur: "Nez"},
+    {nombre: 3, valeur: "Handspinner"},
+    {nombre: 4, valeur: "Rubixcube"},
+    {nombre: 5, valeur: "Cheveux banane"},
+    {nombre: 6, valeur: "Scie"},
+    {nombre: 7, valeur: "Maman"},
+    {nombre: 8, valeur: "Bonbon"},
+    {nombre: 9, valeur: "Papa"},
+    {nombre: 10, valeur: "De"},
     {nombre: 11, valeur: "Millie Bobbie Brown"},
     {nombre: 12, valeur: "Louane"},
     {nombre: 13, valeur: "Mercredi Addams"},
     {nombre: 14, valeur: "Mme Quinconces"},
-    {nombre: 15, valeur: "cake"},
+    {nombre: 15, valeur: "Cake"},
     {nombre: 16, valeur: "Saez"},
     {nombre: 17, valeur: "Stephanie"},
-    {nombre: 18, valeur: "napperon"},
-    {nombre: 19, valeur: "boite oeufs"},
+    {nombre: 18, valeur: "Napperon"},
+    {nombre: 19, valeur: "Boîte oeufs"},
   ];
 
   public static NOMBRE_MAX: number = 19;
@@ -38,14 +38,18 @@ export class AppComponent {
   public question:number = Math.floor(Math.random() * AppComponent.NOMBRE_MAX);
   public oldQuestion:number = -1;
   public commentaire: string = 'A toi de jouer !';
+  public reussites: number = 0;
+  public total: number = 0;
 
   public ngOnInit(): void {
     this.oldQuestion = this.question;
   }
 
   public onValider(): void {
+    this.total++;
     if(this.estLaBonneReponse()) {
       this.commentaire = 'Bravo ! Encore !';
+      this.reussites++;
       this.genererNouvelleQuestion();
       this.reponse ='';
     } else {
