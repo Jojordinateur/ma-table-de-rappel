15be4a4f24dfc8e4932d37438410cb2acbd32962
Sanjd <jojoloiseaumusique@gmail.com>
2023-08-23 16:15:11 +0200

    ajout table rappel liste

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,24 @@
 import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { JeuComposant } from './composants/jeu/jeu';
+import { HomeComposant } from './composants/home/home';
+import { TableRappelComposant } from './composants/table-rappel/table-rappel';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComposant,
+    JeuComposant,
+    TableRappelComposant
   ],
   imports: [
     BrowserModule,
+    CommonModule,
     AppRoutingModule,
     HttpClientModule
   ],
